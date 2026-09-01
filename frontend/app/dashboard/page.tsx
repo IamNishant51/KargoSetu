770cd76d4bcdfe3c2c70afde82204f8de37144a7
IamNishant51 <anonymouslucifer400@gmail.com>
2026-09-01 15:36:24 +0530

    chore: remove frontend and backend directories

--- frontend/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import ExecutiveDashboard from "@/components/ExecutiveDashboard";
-
-export default function DashboardPage() {
-  return (
-    <div className="mx-auto max-w-[1440px] px-6 py-8">
-      <ExecutiveDashboard />
-    </div>
-  );
-}
